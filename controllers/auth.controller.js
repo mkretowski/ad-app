41ab57c698b1f300e4c64a14f10bc2a54a22334a
Mateusz Kretowski <matkretkurs@gmail.com>
2023-06-01 20:40:26 +0200

    Add login endpoint

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -18,4 +18,25 @@ exports.register = async (req, res) => {
   }
 };
 
-exports.login = async (req, res) => {};
+exports.login = async (req, res) => {
+  try {
+    const { login, password } = req.body;
+    if (login && typeof login === 'string' && password && typeof password === 'string') {
+      const user = await User.findOne({ login });
+      if (!user) {
+        res.status(400).send({ message: 'Login or password are incorrect' });
+      } else {
+        if (bcrypt.compareSync(password, user.password)) {
+          res.status(200).send({ message: 'Login successful' });
+        } else {
+          res.status(400).send({ message: 'Login or password are incorrect' });
+        }
+      }
+      //const user = await User.create({ login, password: await bcrypt.hash(password, 10) });
+    } else {
+      res.status(400).send({ message: 'Bad request' });
+    }
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
