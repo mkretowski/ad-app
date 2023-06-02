a8163a110f41d27447bcb04baceb556d6141371b
Mateusz Kretowski <matkretkurs@gmail.com>
2023-06-02 23:33:18 +0200

    Add session registration

--- db.js
+++ db.js
@@ -1,5 +1,24 @@
-db = {};
+const mongoose = require('mongoose');
 
-module.exports = {
-  db,
+const connectToDB = () => {
+  const NODE_ENV = process.env.NODE_ENV;
+  let dbUri = '';
+
+  if (NODE_ENV === 'production') {
+    dbUri = '';
+  } else if (NODE_ENV === 'test') {
+    dbUri = 'mongodb://localhost:27017/adsDBtest';
+  } else {
+    dbUri = 'mongodb://localhost:27017/adsDB';
+  }
+
+  mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
+  const db = mongoose.connection;
+
+  db.once('open', () => {
+    console.log('Connected to the database');
+  });
+  db.on('error', (err) => console.log('Error ' + err));
 };
+
+module.exports = connectToDB;
--- server.js
+++ server.js
@@ -2,19 +2,25 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const connectToDB = require('./db');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const mongoose = require('mongoose');
+
 //start express server
 const app = express();
 app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
 
-//connectToDB();
+connectToDB();
 
 //add middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({ secret: 'xyz567', store: MongoStore.create(mongoose.connection), resave: false, saveUninitialized: false })
+);
 
 // serve static files from the React app
 app.use(express.static(path.join(__dirname, '/client/build')));
@@ -31,25 +37,5 @@ app.get('*', (req, res) => {
 app.use((req, res) => {
   res.status(404).send('404 not found...');
 });
-
-const NODE_ENV = process.env.NODE_ENV;
-console.log(NODE_ENV);
-let dbUri = '';
-
-if (NODE_ENV === 'production') {
-  dbUri = '';
-} else if (NODE_ENV === 'test') {
-  dbUri = 'mongodb://localhost:27017/adsDBtest';
-} else {
-  dbUri = 'mongodb://localhost:27017/adsDB';
-}
-
-mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-
-db.once('open', () => {
-  console.log('Connected to the database');
-});
-db.on('error', (err) => console.log('Error ' + err));
 
 //module.exports = server;
